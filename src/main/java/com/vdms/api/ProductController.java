11cada8008d79024fc97e763e0fa9533e9ffb1f8
Kenji <krudio@gmail.com>
2016-12-02 23:29:41 -0800

    Fix URI mappings and add input validation

--- src/main/java/com/vdms/api/ProductController.java
+++ src/main/java/com/vdms/api/ProductController.java
@@ -1,15 +1,13 @@
 package com.vdms.api;
 
 import com.vdms.api.model.Product;
+import com.vdms.api.services.ProductService;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.Collection;
-import java.util.List;
 
 /**
  * Created by Kenji on 12/1/2016.
@@ -22,7 +20,7 @@ public class ProductController {
     private ProductService productService;
 
     @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Product addProduct(@RequestBody Product product) {
+    public Product addProduct(@Validated @RequestBody Product product) {
         return productService.addProduct(product);
     }
 
@@ -31,13 +29,13 @@ public class ProductController {
         return productService.getAllProducts();
     }
 
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Product getProductByCode(@RequestBody String code) {
+    @RequestMapping(value = "/code/{code}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Product getProductByCode(@PathVariable String code) {
         return productService.getProductByCode(code);
     }
 
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Collection<Product> getProductsByTag(@RequestBody String tag) {
+    @RequestMapping(value = "/tag/{tag}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Collection<Product> getProductsByTag(@PathVariable String tag) {
         return productService.getProductsByTag(tag);
     }
 }
